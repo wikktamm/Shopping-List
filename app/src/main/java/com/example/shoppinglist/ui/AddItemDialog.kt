8415555ca8122c2f1dd8a2d4c16971f29f19dc9e
Wiktoria Tarnowska <wikktamm@gmail.com>
2020-06-04 18:18:25 +0200

    Display dialog for adding items; ui changes; extract string resources

--- app/src/main/java/com/example/shoppinglist/ui/AddItemDialog.kt
+++ app/src/main/java/com/example/shoppinglist/ui/AddItemDialog.kt
@@ -2,25 +2,29 @@ package com.example.shoppinglist.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDialog
+import com.example.shoppinglist.R
 import com.example.shoppinglist.models.ShoppingItem
+import com.example.shoppinglist.other.getIntContent
+import com.example.shoppinglist.other.getStringContent
 import kotlinx.android.synthetic.main.dialog_add_item.*
 
 class AddItemDialog(context: Context, val listener: AddDialogListener) : AppCompatDialog(context) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_add_item)
         bt_accept.setOnClickListener {
             if (et_name.text.isEmpty() || et_count.text.isEmpty()) {
                 Toast.makeText(context, "You need to enter the required data!", Toast.LENGTH_SHORT)
                     .show()
                 return@setOnClickListener
             }
-            val nameInput = et_name.text.toString()
-            val countInput = et_count.text.toString().toInt()
+            val nameInput = et_name.getStringContent()
+            val countInput = et_count.getIntContent()
             val itemToInsert = ShoppingItem(nameInput, countInput)
             listener.onAcceptListener(itemToInsert)
+            dismiss()
         }
         bt_cancel.setOnClickListener { cancel() }
     }
--- app/src/main/java/com/example/shoppinglist/ui/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/ui/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.shoppinglist.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shoppinglist.R
 import com.example.shoppinglist.db.ShoppingDatabase
 import com.example.shoppinglist.db.ShoppingListRepository
+import com.example.shoppinglist.models.ShoppingItem
 import com.example.shoppinglist.other.ShoppingItemAdapter
 import com.example.shoppinglist.viewmodels.ShoppingListViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShoppingListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +24,16 @@ class MainActivity : AppCompatActivity() {
         val db = ShoppingDatabase(this)
         val repo = ShoppingListRepository(db)
         val factory = ShoppingViewModelFactory(repo)
-        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingListViewModel::class.java)
-        setRecyclerViewAndFetchData(viewModel, repo)
+        viewModel = ViewModelProviders.of(this, factory).get(ShoppingListViewModel::class.java)
+        setRecyclerViewAndLiveData(viewModel, repo)
+        setListeners()
+        title = getString(R.string.my_shopping_list).toUpperCase()
     }
 
-    private fun setRecyclerViewAndFetchData(vm: ShoppingListViewModel, repo: ShoppingListRepository) {
+    private fun setRecyclerViewAndLiveData(
+        vm: ShoppingListViewModel,
+        repo: ShoppingListRepository
+    ) {
         val adapter = ShoppingItemAdapter(listOf(), vm)
         rv_items.layoutManager = LinearLayoutManager(this)
         rv_items.adapter = adapter
@@ -34,5 +41,18 @@ class MainActivity : AppCompatActivity() {
             adapter.items = it
             adapter.notifyDataSetChanged()
         })
+    }
+
+    private fun setListeners() {
+        fab_add.setOnClickListener {
+            val dialog = AddItemDialog(this, object : AddDialogListener {
+                override fun onAcceptListener(item: ShoppingItem) {
+                    viewModel.upsert(item)
+                }
+            })
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setTitle(getString(R.string.add_new_title).toUpperCase())
+            dialog.show()
+        }
     }
 }
